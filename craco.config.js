06537d5b9165ea7cb0505f27dc902373c6a81232
xiangzhengfeng <eoszhengfeng@163.com>
2021-08-30 13:30:08 +0800

    人生何曾驻旧足，前端处处是新旅

--- craco.config.js
+++ craco.config.js
@@ -1,11 +1,22 @@
 // craco.config.js
 module.exports = {
-    style: {
-      postcss: {
-        plugins: [
-          require('tailwindcss'),
-          require('autoprefixer'),
-        ],
-      },
+  style: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
     },
+  },
+  webpack: {
+    // 以下代码！！！  与alias或babel同级
+    configure: (webpackConfig, { env, paths }) => {
+      // 修改build的生成文件名称
+      webpackConfig.output = {
+        ...webpackConfig.output,
+        publicPath: '/PixelMaster/'
+      }
+      return webpackConfig;
+    }
   }
+}
