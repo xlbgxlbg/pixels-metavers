3a96048f166cad1aaa4ff5194071859232a178c0
xiangzhengfeng <eoszhengfeng@163.com>
2021-08-30 12:57:20 +0800

    人生何曾驻旧足，前端处处是新旅

--- src/a.tsx
+++ src/a.tsx
@@ -1,9 +1,13 @@
 import { useEffect } from "react"
 import { getTableDataList } from "./api/fetch"
 
-export const A = ({a}:{a: string}) => {
+export const A = ({ a }: { a: string }) => {
     useEffect(() => {
         getTableDataList("", 20)
     }, [])
-    return <div>{a}</div>
+    return <div className="mt-3 sm:mt-0 sm:ml-3 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+        <a href="#" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 hover:text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
+            Live demo123
+      </a>
+    </div>
 }
